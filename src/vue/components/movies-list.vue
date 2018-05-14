6e18c436831fe1998fb1c308796f4f590d6d9537
Sandking1123 <nikolain1203@gmail.com>
2018-05-14 16:18:56 +0200

    recherche de film

--- src/vue/components/movies-list.vue
+++ src/vue/components/movies-list.vue
@@ -3,8 +3,15 @@
         <h1>Liste des films</h1>
         <hr>
         <br/>
+        <v-text-field
+                solo-inverted
+                flat
+                label="Rechercher"
+                prepend-icon="search"
+                v-model="search"
+        ></v-text-field>
         <div class="movies-list">
-            <movie-item v-for="movie in movies" v-bind:movie="movie"></movie-item>
+            <movie-item v-for="(movie, index) in movies_search" v-bind:movie="movie"></movie-item>
         </div>
     </v-flex>
 </template>
@@ -14,11 +21,16 @@
 
     export default {
         data () {
-            return { }
+            return {
+                search: "",
+            }
         },
         computed: mapState ({
             movies: function() {
                 return this.$store.state.movies;
+            },
+            movies_search: function() {
+                return this.movies.filter(m => m.title.toLowerCase().indexOf(this.search.toLowerCase())!==-1);
             }
         }),
         mounted: function() {
